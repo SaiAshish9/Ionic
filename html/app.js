46b4fd17625cf75189c5c2df4d8ced8e247e2876
SaiAshish9 <saiashish3760@gmail.com>
2020-09-06 12:54:23 +0530

    template updated

---
+++ html/app.js
@@ -0,0 +1,6 @@
+const reasonInput = document.querySelector("#input-reason");
+const confirmBtn = document.querySelector("#btn-confirm");
+
+confirmBtn.addEventListener("click", () => {
+  console.log("it works");
+});
